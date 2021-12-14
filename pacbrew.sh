e2aa79ac2d106fc5d907a31642a9160d8b8ee2f4
cpasjuste <cpasjuste@gmail.com>
2021-12-14 11:04:07 +0100

    pacbrew: first commit

---
+++ pacbrew.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# vars
+PACBREW=/opt/pacbrew
+
+# deps
+sudo apt install -y meson ninja-build gettext libarchive-dev libcurl4-openssl-dev libgpgme-dev
+
+# pacbrew build
+rm -rf build
+meson --prefix=$PACBREW/pacman --sysconfdir=$PACBREW/pacman/etc --localstatedir=$PACBREW/pacman/var -Droot-dir=$PACBREW/pacman -Dmakepkg-template-dir=$PACBREW/usr/share/makepkg-template build/
+ninja -C build/
+ninja -C build/ install
+
+# pacbrew links
+sudo cp pacbrew/bin/* /usr/bin/
+
