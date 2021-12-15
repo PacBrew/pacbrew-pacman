f90585331f23bc6df8dd1838e07ba4f2f449d6f8
Cpasjuste <cpasjuste@gmail.com>
2021-12-15 22:32:49 +0100

    fix install

--- pacbrew.sh
+++ pacbrew.sh
@@ -10,7 +10,7 @@ sudo apt install -y meson ninja-build gettext libarchive-dev libcurl4-openssl-de
 rm -rf build
 meson --prefix=$PACBREW/pacman --sysconfdir=$PACBREW/pacman/etc --localstatedir=$PACBREW/pacman/var -Dpkg-ext=.pkg.tar.xz -Dmakepkg-template-dir=$PACBREW/usr/share/makepkg-template build/
 ninja -C build/
-ninja -C build/ install
+sudo ninja -C build/ install
 
 # pacbrew links
 sudo cp pacbrew/bin/* /usr/bin/
