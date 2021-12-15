4a5317a377225a2879c68c1d7a238d592819571a
cpasjuste <cpasjuste@gmail.com>
2021-12-15 08:08:28 +0100

    pacbrew: use xz compression

--- pacbrew.sh
+++ pacbrew.sh
@@ -8,7 +8,7 @@ sudo apt install -y meson ninja-build gettext libarchive-dev libcurl4-openssl-de
 
 # pacbrew build
 rm -rf build
-meson --prefix=$PACBREW/pacman --sysconfdir=$PACBREW/pacman/etc --localstatedir=$PACBREW/pacman/var -Dmakepkg-template-dir=$PACBREW/usr/share/makepkg-template build/
+meson --prefix=$PACBREW/pacman --sysconfdir=$PACBREW/pacman/etc --localstatedir=$PACBREW/pacman/var -Dpkg-ext=.pkg.tar.xz -Dmakepkg-template-dir=$PACBREW/usr/share/makepkg-template build/
 ninja -C build/
 ninja -C build/ install
 
